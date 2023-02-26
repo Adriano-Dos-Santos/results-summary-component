81a33bbe32fde6907f36dc983f93df129e1455af
Adriano Dos Santos <adsc999@gmail.com>
2023-02-26 09:34:32 -0300

    Project completed

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
     for (let i = 0; i <= data.length - 1; i++) {
       const spanElement = document.getElementById(`score-${i + 1}`);
       spanElement.textContent = data[i].score;
-    }});
+    }
+  });
